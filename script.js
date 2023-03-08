fad4f863dd10c85d642331f6e57b622607d28d6e
ZellyJelly <zellexepc@gmail.com>
2023-03-08 06:57:46 -0500

    added functions to create custom grids

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+let columns = 1;
+let blocks = 1;
+let userChoice;
 
+
+
+buildGrid();
+
+async function buildGrid(){
+
+    userChoice = await userQuestion();
+    await createColumns(userChoice);
+    await createBlocks(userChoice);
+}
+
+
+function userQuestion(choice){
+
+    choice = prompt("How big would you like your grid?"); 
+    return choice;
+}
+
+function createColumns(choice)
+{
+    let max = choice; 
+    gridContainer = document.querySelector('.gridContainer') 
+
+    for (let i = 1; i < choice; i++){
+        const div = document.createElement('div');
+        div.dataset.container = `${i}`;
+        gridContainer.appendChild(div);
+        columns++
+    }
+}
+
+function createBlocks(choice)
+{
+    let currentColumn = 0;
+
+    for(let i = 0; i < choice; i++){
+        gridCurrentContainer = document.querySelector(`[data-container="${currentColumn}"`)
+        currentColumn++
+        for(let i = 0; i < choice; i++){
+            const div = document.createElement('div');
+            div.dataset.blocks = `${i}`;
+            gridCurrentContainer.appendChild(div);
+
+        }
+    }
+}
